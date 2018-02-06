3b3b0fc0fff2c36cc5f1981d45029e899f30f759
刘文超 <liuwenchao1990@163.com>
2018-02-06 15:07:55 +0800

    修改首页代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,18 @@
 //获取应用实例
 const app = getApp()
 Page({
-  data: {}
+  data: {},
+  onShow:function(){
+  	wx.request({
+  		url: "http://127.0.0.1:8000/category/list",
+  		dataType: "json",
+  		method: "post",
+  		success: function(res){
+  			console.log(res)
+  		},
+  		fail: function(error){
+  			console.log(error);
+  		}
+  	})
+  }
 })
